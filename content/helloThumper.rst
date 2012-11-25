f48c3d253e83c4f542873347fc32aa8eaafc9729
JakeMick <jam7w2@mail.missouri.edu>
2012-11-25 16:51:21 -0600

    initial commit of pelican powered blog

---
+++ content/helloThumper.rst
@@ -0,0 +1,9 @@
+Hello, Thumper!
+###############
+
+:date: 2012-11-25 15:04
+:tags: peace, love, fishsticks
+:category: toy
+:author: Jake Mick
+
+test post :abbr:`herp`.
---
+++ pelicanconf.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u"jakeMick"
+SITENAME = u"numpy|Thumper"
+SITEURL = ''
+
+TIMEZONE = 'America/Chicago'
+
+DEFAULT_LANG = 'en'
+
+# Blogroll
+LINKS = (('R-bloggers', 'http://www.r-bloggers.com/'),
+         ('My Experiments in Truth', 'http://izbicki.me/blog/'),
+         ('decomposition', 'http://seanjtaylor.com/'),
+         ('mathbabe', 'http://mathbabe.org/'),
+         )
+
+# Social widget
+SOCIAL = (('gitHub', 'http://github.com/JakeMick'),
+          ('linkedIn', 'http://www.linkedin.com/pub/jacob-mick/53/2b2/b38'),
+          )
+
+DEFAULT_PAGINATION = 10
